2785a9e2e7194cad88a9d0a95d42bd0bc548989c
Petr Mlejnek <mlejnpe1@uhk.cz>
2025-08-16 13:40:04 +0200

    routes structure + routes controller

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from "express";
+import notesRoutes from "./routes/noteRoutes.js"
 
 const app = express();
 
-app.get("/api/notes", (req,res) =>{
-    res.status(200).send("you got 5 notes..");
-})
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () =>{
     console.log("Server started on PORT: 5001");
