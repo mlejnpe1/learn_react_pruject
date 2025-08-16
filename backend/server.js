abf8ab95c71e80de3925e2da29ac68e25eb8ae66
Petr Mlejnek <mlejnpe1@uhk.cz>
2025-08-16 12:55:23 +0200

    nodemon added

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,6 @@ app.get("/api/notes", (req,res) =>{
     res.status(200).send("you got 5 notes..");
 })
 
-app.post("/api/notes", (req,res) =>{
-    res.status(201).send("your note has been created succesfully..");
-})
-
 app.listen(5001, () =>{
     console.log("Server started on PORT: 5001");
 });
