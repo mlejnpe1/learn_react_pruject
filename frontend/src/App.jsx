38043587240ddd541d02cd47bbd747e973d7c0b6
Petr Mlejnek <mlejnpe1@uhk.cz>
2025-08-17 11:30:51 +0200

    tailwind instalation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/create' element={<CreatePage/>}></Route>
+        <Route path='/note/:id' element={<NoteDetailPage/>}></Route>
+      </Routes>
+    </div>
   )
 }
 
