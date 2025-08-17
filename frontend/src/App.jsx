26aaa33ac9f164888a3b4e7ef097d53bc861f17e
Petr Mlejnek <mlejnpe1@uhk.cz>
2025-08-17 11:53:04 +0200

    tailwind theme setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
-import CreatePage from './pages/CreatePage'
-import NoteDetailPage from './pages/NoteDetailPage'
-import toast from 'react-hot-toast'
+import React from "react";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
+      <div data-theme='autumn' />
+      <button className='btn'>Button</button>
+      <button className='btn btn-neutral'>Neutral</button>
+      <button className='btn btn-primary'>Primary</button>
+      <button className='btn btn-secondary'>Secondary</button>
+      <button className='btn btn-accent'>Accent</button>
+      <button className='btn btn-ghost'>Ghost</button>
+      <button className='btn btn-link'>Link</button>
       <Routes>
-        <Route path='/' element={<HomePage/>}></Route>
-        <Route path='/create' element={<CreatePage/>}></Route>
-        <Route path='/note/:id' element={<NoteDetailPage/>}></Route>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/create' element={<CreatePage />}></Route>
+        <Route path='/note/:id' element={<NoteDetailPage />}></Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
