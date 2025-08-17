f30a16394fd50bef374f0cbbcdfabc1fb62f9988
Petr Mlejnek <mlejnpe1@uhk.cz>
2025-08-17 22:15:51 +0200

    homepage layout

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,12 +1,12 @@
-import {Ratelimit} from "@upstash/ratelimit"
-import { Redis } from "@upstash/redis"
-import dotenv from "dotenv"
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(100, "60 s")
-})
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "20 s"),
+});
 
-export default ratelimit
+export default ratelimit;
